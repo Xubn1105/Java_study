bc1be1d3d45d4567a1d4a0a3fe88712ca5b2c641
user <user@userdeMacBook-Pro.local>
2021-07-08 19:11:50 +0800

    Inner class

---
+++ src/part2/Innerclass.java
@@ -0,0 +1,23 @@
+package part2;
+
+public class Innerclass{
+    public static void main(String[] args){
+        Outer outer = new Outer("liming");
+        Outer.Inner inner = outer.new Inner();
+        inner.hello();
+    }
+}
+
+class Outer{
+    private String name;
+
+    public Outer(String name){
+        this.name = name;
+    }
+
+    class Inner{
+        void hello(){
+            System.out.println("Hello, " + Outer.this.name);
+        }
+    }
+}
