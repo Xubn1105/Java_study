2fa1b4e3263f2965801b937e2921df82ff8d3949
user <user@userdeMacBook-Pro.local>
2021-07-08 16:48:44 +0800

    test

--- src/part2/Static.java
+++ src/part2/Static.java
@@ -1,5 +1,24 @@
 package part2;
 
-public class Static
-{
+public class Static {
+    public static void main(String[] args){
+        Personn m = new Personn("xiaoming", 12);
+        Personn n = new Personn("lihong", 15);
+        m.number = 88;
+        System.out.println(n.number);
+        n.number = 99;
+        System.out.println(m.number);
+    }
+}
+
+class Personn{
+    private String name;
+    private int age;
+
+    public static int number;
+
+    public Personn(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
 }
