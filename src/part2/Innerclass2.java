0d54c7e1c092e3dfa1289c7ef5e85ff2029741e2
user <user@userdeMacBook-Pro.local>
2021-07-08 19:36:27 +0800

    Inner class 2

--- src/part2/Innerclass2.java
+++ src/part2/Innerclass2.java
@@ -1,2 +1,24 @@
-package part2;public class Innerclass2 {
+package part2;
+
+public class Innerclass2 {
+    public static void main(String[] args){
+
+    }
+}
+
+class Outerrr{
+    private static String NAME = "OUTER";
+
+    private String name;
+
+    public Outerrr(String name){
+        this.name = name;
+    }
+
+    //静态内部类可以访问外部类的 private静态字段和静态方法
+    static class StaticNested {
+        void hello(){
+            System.out.println("Hello, " + Outerrr.NAME);
+        }
+    }
 }
